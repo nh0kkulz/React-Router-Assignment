d02e2edddba9856a0991ad4adfdb0c76a2633a00
Trong Pham <trumhentaidenhat@gmail.com>
2021-04-17 07:59:12 -0400

    Finished All

---
+++ src/containers/Course/Course.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class Course extends Component {
+    state = {
+        title: ''
+    }
+
+    componentDidMount() {
+        const query = new URLSearchParams(this.props.location.search);
+        for (let param of query.entries()) {
+            
+            this.setState({title: param[1]});
+        }
+    }
+
+    componentDidUpdate() {
+        const query = new URLSearchParams(this.props.location.search);
+        for (let param of query.entries()) {
+            if(this.state.title !== param[1])
+                this.setState({title: param[1]});
+        }
+    }
+
+    render () {
+        return (
+            <div>
+                <h1>{this.state.title}</h1>
+                <p>You selected the Course with ID: {this.props.match.params.courseId}</p>
+            </div>
+        );
+    }
+}
+
+export default Course;
